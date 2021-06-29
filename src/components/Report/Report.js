8fc9da47332f7042bfac5bc019f97ae7c59924ba
manooi <sirawit.mn@gmail.com>
2021-06-30 03:55:28 +0700

    Add DELETE for /parcel/:parcelId

--- src/components/Report/Report.js
+++ src/components/Report/Report.js
@@ -1,29 +1,73 @@
 import dummyReports from "./dummyReports.js";
-import React, { useState } from 'react'
+import NumberFormat from 'react-number-format';
+import React, { useState, useEffect } from 'react'
 import './Report.css';
 import RandomHr from '../RandomHr/RandomHr';
 const axios = require('axios').default;
 
-
 function Report() {
+
   document.title = `Report | Saleng.th`;
   const [reports, setReport] = useState([]);
+  const [isInitialFetch, setIsinitialFetch] = useState(false);
 
+  function ActualReports(props) {
+
+    return (
+      <tr>
+        <td>{props._id}</td>
+        <td>{props.senderFName}</td>
+        <td>{props.recipientFName}</td>
+        <td style={{ backgroundColor: props.deliverStatus === "Success" ? 'lime' : 'none' }}>{props.deliverStatus}</td>
+        <td><NumberFormat value={props.parcelCost} displayType={'text'} thousandSeparator={true} prefix={'THB'} /></td>
+        <td><button onClick={() => { props.deleteParcel(props._id) }}>Del</button></td>
+      </tr>
+    )
+  }
+
+  // Fetch
   async function fetchParcel() {
     const response = await axios.get("/parcel");
     setReport(response.data);
     // console.log(response.data);
     console.log("Fetching data...")
+    setIsinitialFetch(true);
   }
 
-  // Fetch data for the first time!
-  const ALWAYS_FETCH = false
+  // Delete
+  async function handleDelete(_id) {
+    try {
+      await axios.delete('/parcel/' + _id, {
+        _id: _id
+      });
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
-  if (ALWAYS_FETCH ? reports : reports.length === 0) {
-    console.log("No report");
+  function deleteParcel(id) {
+    // console.log("Called")
+    // console.log(`id is ${id}`)
+    const _id = id
+    handleDelete(_id);
+    fetchParcel();
+    // setReport(prevReports => {
+    //   return prevReports.filter((report, index) => {
+    //     return index !== id
+    //   });
+    // });
+  }
+
+  // Initial fetch
+  if (!isInitialFetch) {
     fetchParcel();
   }
 
+  // DOESN'T WORK!
+  // Set timer (fetch every 5 sec)
+  // setInterval(() => {
+  //   setIsinitialFetch(false)
+  // }, 5000);
 
   return (
     <div class="report_container">
@@ -36,8 +80,6 @@ function Report() {
       <div>
         <h2>All parcels</h2>
       </div>
-
-      {/* How to do auto refresh?? */}
       <button onClick={fetchParcel} style={{ marginBottom: '10px' }}>Refresh</button>
 
       <table>
@@ -47,6 +89,7 @@ function Report() {
           <th>Recipient</th>
           <th>Status</th>
           <th>Cost</th>
+          <th>Delete</th>
         </tr>
 
         {dummyReports.map((report, index) => {
@@ -56,32 +99,31 @@ function Report() {
             <td>{report.recipient}</td>
             <td style={{ backgroundColor: report.deliverStatus === "Success" ? 'lime' : 'none' }}>{report.deliverStatus}</td>
             <td>{report.cost}</td>
+            <td><button>Del</button></td>
           </tr>
           )
         })}
 
-        {reports ? reports.map((report, index) => {
+        {reports.map((report, index) => {
           return (
-            <tr>
-              <td>{report._id}</td>
-              <td>{report.sender.firstName}</td>
-              <td>{report.recipient.firstName}</td>
-              <td style={{ backgroundColor: report.deliverStatus === "Success" ? 'lime' : 'none' }}>{report.deliverStatus}</td>
-              <td>{report.parcel.cost}</td>
-            </tr>
-            /*<Note
+            <ActualReports
               key={index}
               id={index}
-              title={noteItem.title}
-              content={noteItem.content}
-              onDelete={deleteNote}
-            />  */
+              _id={report._id}
+              senderFName={report.sender.firstName}
+              recipientFName={report.recipient.firstName}
+              deliverStatus={report.deliverStatus}
+              parcelCost={report.parcel.cost}
+              deleteParcel={deleteParcel}
+            />
           );
-        }) : 0}
+        })}
+        {/* <button onClick={deleteParcel} >Test</button> */}
       </table>
-
     </div>
   )
 }
 
 export default Report
+
+
